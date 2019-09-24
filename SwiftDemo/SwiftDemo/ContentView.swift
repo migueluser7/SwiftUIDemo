c958925115d26673c9644850be6926dd0d9751e8
pursuit <pursuit@MB-pro.local>
2019-09-24 15:51:49 -0400

    added xcode project

---
+++ SwiftDemo/SwiftDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftDemo
+//
+//  Created by pursuit on 9/24/19.
+//  Copyright © 2019 Miguel Aquino. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World this the new display")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
